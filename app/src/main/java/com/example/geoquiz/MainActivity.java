b3d849689aa83e9156351b32daf327539df1f007
pintaste <lxsmyq@gmail.com>
2020-09-27 19:54:30 -0700

    LogCat config

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
     private TextView mQuestionTextView;
 
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -30,9 +32,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // log output method
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+        // challenge 2.7: set a Listener for questionTextView
+        // go to the next question when clicked
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +64,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // challenge 2.8 add a PREV button
+        // go back last question when clicked
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex > 0){
+                    mCurrentIndex--;
+                }
+                else {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
+                updateQuestion();
+            }
+        });
+
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,49 +88,44 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-
-        updateQuestion();
-
-        // challenge 2.7: set a Listener for questionTextView
-        // go to the next question when clicked
-        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-
-        // challenge 2.8 add a PREV button
-        // go back last question when clicked
-        mPrevButton = (Button) findViewById(R.id.prev_button);
-        mPrevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-
     }
-        private void updateQuestion() {
-            int question = mQuestionBank[mCurrentIndex].getTextResId();
-            mQuestionTextView.setText(question);
-        }
 
-        private void checkAnswer(boolean userPressedTrue) {
-            boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-            int messageResId = 0;
-            if (userPressedTrue == answerIsTrue) {
-                messageResId = R.string.correct_toast;
-            } else {
-                messageResId = R.string.incorrect_toast;
-            }
-            Toast t = Toast.makeText(this,messageResId, Toast.LENGTH_SHORT);
-            // challenge 1.11: show the toast message at top of screen.
-            t.setGravity(Gravity.TOP,0,0);
-            t.show();
-
-        }
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        // challenge 1.11: show the toast message at top of screen.
+        t.setGravity(Gravity.TOP, 0, 0);
+        t.show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+}
+
 
